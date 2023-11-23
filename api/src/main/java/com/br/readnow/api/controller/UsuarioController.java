e401e93ade27cfd9d7b4207e08310c87935cadef
victoriacardoso <victoriacardoso82@gmail.com>
2023-11-22 22:08:04 -0300

    integracao da tela inicial e de login

--- api/src/main/java/com/br/readnow/api/controller/UsuarioController.java
+++ api/src/main/java/com/br/readnow/api/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.br.readnow.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +13,18 @@ import com.br.readnow.api.service.UsuarioService;
 import com.br.readnow.api.dto.LoginDTO;
 import com.br.readnow.api.dto.UsuarioDTO;
 
-@RestController
+@Controller
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping("/login")
-    public ResponseEntity<UsuarioDTO> efetuarLogin(@RequestBody LoginDTO login){
-        return usuarioService.login(login);
+    public String telaLogin() {
+        return "login";
     }
+    // public ResponseEntity<UsuarioDTO> efetuarLogin(@RequestBody LoginDTO login){
+    //     return usuarioService.login(login);
+    // }
     
     @PostMapping("/cadastro")
     public ResponseEntity<?> cadastrarUsuario(@RequestBody UsuarioModel usuario){
