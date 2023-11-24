42261401549fad0afa588f7bbd68ae195f3cde43
Nagathi <ragbr7070@gmail.com>
2023-11-24 17:42:58 -0300

    Rota de envio de email alterada, modificações no HTML do formulário de recuperar senha. Adicionado DTO para e-mail

---
+++ api/src/main/java/com/br/readnow/api/dto/EmailDTO.java
@@ -0,0 +1,10 @@
+package com.br.readnow.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailDTO {
+    private String email;
+}
---
+++ api/src/main/resources/static/js/enviaEmail.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btnEnviarEmail = document.querySelector('.btn-submit');
+  
+    btnEnviarEmail.addEventListener('click', function() {
+      const email = document.querySelector('input[type="text"]').value;
+  
+      if (email.trim() !== '') {
+        const data = { email };
+  
+        fetch('/enviaEmail', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then(response => {
+          if (response.ok) {
+            showModal("Email enviado com sucesso!");
+          } else if (response.status === 404) {
+            showModal("E-mail não encontrado!");
+          } else {
+            throw new Error('Falha ao enviar o email');
+          }
+        })
+        .catch(error => {
+          showModal('Erro ao enviar o email: ' + error.message);
+        });
+      } else {
+        showModal('Por favor, preencha o campo de email.');
+      }
+    });
+  
+    function showModal(message) {
+      const modal = document.getElementById("modal");
+      const modalMessage = document.getElementById("modal-message");
+      const closeButton = document.querySelector(".close");
+  
+      modalMessage.textContent = message;
+      modal.style.display = "block";
+  
+      closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+      });
+  
+      window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+          modal.style.display = "none";
+        }
+      });
+    }
+  });
+  
