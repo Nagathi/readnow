30f59274f6f6c0c11e31de87dea5090ff040f53d
victoriacardoso <victoriacardoso82@gmail.com>
2023-12-02 21:03:37 -0300

    header principal ajustado

--- api/src/main/resources/static/components/header-principal.js
+++ api/src/main/resources/static/components/header-principal.js
@@ -1,6 +1,5 @@
 (() => {
 
-<<<<<<< HEAD
     class HeaderPrincipal extends HTMLElement {
       constructor() {
         super();
@@ -36,62 +35,3 @@
     customElements.define('header-principal', HeaderPrincipal);
   
   })();
-=======
-  class HeaderPrincipal extends HTMLElement {
-    constructor() {
-      super();
-    }
-
-    connectedCallback() {
-      this.innerHTML = `
-
-        <header class="cabecalho">
-          <a href="/">
-            <img src="images/icons/logo.svg" alt="Logo" class="logo">
-          </a>
-          
-          <div class="box-search">
-            <input type="text" placeholder="O que deseja encontrar?" class="box-input">
-            <button class="button-search">
-              <img src="images/icons/lupa.png" alt="Lupa">
-            </button>
-          </div>
-      
-          <nav class="opcoes">
-            <button class="item">
-              <a href="#">
-                <img src="images/icons/user.svg" alt="Carrinho de compras" style="width: 3.6rem ;">
-                <span class="identificador">Sua conta</span>
-              </a>
-            </button>
-      
-            <button class="item">
-              <a href="#">
-                <img src="images/icons/ouvidoria.svg" alt="Carrinho de compras" style="width: 3.6rem ;">
-                <span class="identificador">Ouvidoria</span>
-              </a>
-            </button>
-      
-            <button class="item" id="carrinho-compras">
-              <a href="#">
-                <img src="images/icons/iconCarrier.svg" alt="Carrinho de compras" style="width: 3.6rem ;">
-                <span class="identificador">Carrinho</span>
-      
-                <div class="produtos-carrinho">
-                  <span class="quantidade">9</span>
-                  <span class="simbolo-mais">+</span>
-                </div>
-              </a>
-            </button>
-          </nav>
-        </header>
-
-      `;
-    }
-
-  }
-
-  customElements.define('header-principal', HeaderPrincipal);
-
-})();
->>>>>>> b13f313510822fd39e8464aee9f2ba289588af36
