065cd5e109b94aa3ab1522f20cff0b02baf477ab
Davi Barroso <daviinformatica228@gmail.com>
2023-12-08 01:31:36 -0300

    Update avaliacao-produto
    Foi atualizado a tela avaliacao-produto.html, avaliacao.js, avaliacao.css e também foi acrescentado mais uma icone, a imagem anexar-imagem.png

--- api/src/main/resources/static/js/avaliacao.js
+++ api/src/main/resources/static/js/avaliacao.js
@@ -1,12 +1,33 @@
 var stars = document.querySelectorAll('.star-icon');
-                  
-document.addEventListener('click', function(e){
+
+document.addEventListener('click', function (e) {
   var classStar = e.target.classList;
-  if(!classStar.contains('ativo')){
-    stars.forEach(function(star){
+  if (!classStar.contains('ativo')) {
+    stars.forEach(function (star) {
       star.classList.remove('ativo');
     });
     classStar.add('ativo');
     console.log(e.target.getAttribute('data-avaliacao'));
   }
 });
+document.getElementById('selectImageButton').addEventListener('click', function () {
+  document.getElementById('fileInput').click();
+});
+
+function handleFileSelect(event) {
+  const fileInput = event.target;
+  const selectedImageContainer = document.getElementById('selectedImageContainer');
+  const selectedImage = document.getElementById('selectedImage');
+
+  if (fileInput.files && fileInput.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      selectedImage.src = e.target.result;
+      selectedImageContainer.style.display = 'block';
+    };
+
+    reader.readAsDataURL(fileInput.files[0]);
+  }
+}
+
